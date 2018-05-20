b020e0e57a05628fdda4b8fb249e39b2bbbdf753
Christian7007 <christig@ucm.es>
2018-05-20 13:06:29 +0200

    Update proyect.m of SVM
    Co-authored-by: jgarriuni <jgarri04@ucm.es>

--- supportvectormachine/proyecto.m
+++ supportvectormachine/proyecto.m
@@ -1,42 +1,35 @@
-X = dlmread('../xtrain.csv');
-y = dlmread('../ytrain.csv');
-X = X(:,2:end);
-y = y(:,2:end);
-% plot with SVM con C = 1
-C = 1;
-model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
-accu = 1 - sum(abs(pred - y)) / length(y)
+X_train = dlmread('../datos/xtrain.csv');
+y_train = dlmread('../datos/ytrain.csv');
 
-% plot with SVM con C = 100
-C = 100;
-model = svmTrain(X, y, C, @linearKernel, 1e-3, 20);
-accu = 1 - sum(abs(pred - y)) / length(y)
+X_val = dlmread('../datos/xval.csv');
+y_val = dlmread('../datos/yval.csv');
 
-% Kernel gausiano
-C = 1;
-sigma = 0.1;
-model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-accu = 1 - sum(abs(pred - y)) / length(y)
+X_test = dlmread('../datos/xtest.csv');
+y_test = dlmread('../datos/ytest.csv');
 
 %% Eleccion de los parametros C y sigma
-%values = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
-%maxAccu = 0;
-%auxC = 0;
-%auxSigma = 0;
-%for i = 1:columns(values)
-%	sigma = values(i);
-%	for j = 1:columns(values)
-%		C = values(j);
-%		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-%		pred = svmPredict(model, Xval);
-%		accu = 1 - sum(abs(pred - yval)) / length(yval);
-%		if accu > maxAccu
-%			maxAccu = accu;
-%			auxC = C;
-%			auxSigma = sigma;
-%		end
-%	end
-%end
-%fprintf("Los mejores son maxAccu -> %f, C -> %f, sigma -> %f\n", maxAccu, auxC, auxSigma);
-%model = svmTrain(X, y, auxC, @(x1, x2) gaussianKernel(x1, x2, auxSigma));
-%model = svmTrain(X, y, 1, @(x1, x2) gaussianKernel(x1, x2, 0.1));
+values = [0.01, 0.1, 1];
+maxAccu = 0;
+auxC = 0;
+auxSigma = 0;
+for i = 1:columns(values)
+	sigma = values(i);
+	for j = 1:columns(values)
+		C = values(j);
+		model = svmTrain(X_train, y_train, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		pred = svmPredict(model, X_val);
+		accu = 1 - sum(abs(pred - y_val)) / length(y_val);
+		if accu > maxAccu
+			maxAccu = accu;
+			auxC = C;
+			auxSigma = sigma;
+		end
+	end
+	fprintf("Test: C %f sigma %f accu %f\n", C, sigma, accu);
+end
+
+fprintf("Los mejores son maxAccu -> %f, C -> %f, sigma -> %f\n", maxAccu, auxC, auxSigma);
+model = svmTrain(X_train, y_train, auxC, @(x1, x2) gaussianKernel(x1, x2, auxSigma));
+pred = svmPredict(model, X_val);
+accu = 1 - sum(abs(pred - y_val)) / length(y_val);
+fprintf("Accuracy %f with C = %f and sigma = %f\n", accu, auxC, auxSigma);
