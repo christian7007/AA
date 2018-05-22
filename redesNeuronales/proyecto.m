145b0fde6383444a7a61fdda12b63fbcbc944658
jgarriuni <jgarri04@ucm.es>
2018-05-22 21:38:05 +0200

    Finish main
    Co-authored-by: christian7007 <christig@ucm.es>

--- redesNeuronales/proyecto.m
+++ redesNeuronales/proyecto.m
@@ -1,18 +1,18 @@
-X_train = dlmread('../datos/xtrain.csv');
-y_train = dlmread('../datos/ytrain.csv');
+X_train = dlmread('../DatosAgrupados/xtrain.csv');
+y_train = dlmread('../DatosAgrupados/ytrain.csv');
 
-X_val = dlmread('../datos/xval.csv');
-y_val = dlmread('../datos/yval.csv');
+X_val = dlmread('../DatosAgrupados/xval.csv');
+y_val = dlmread('../DatosAgrupados/yval.csv');
 
-X_test = dlmread('../datos/xtest.csv');
-y_test = dlmread('../datos/ytest.csv');
+X_test = dlmread('../DatosAgrupados/xtest.csv');
+y_test = dlmread('../DatosAgrupados/ytest.csv');
 
 y_train = y_train.+1;
 y_val = y_val.+1;
 y_test = y_test.+1;
 
 num_entradas = columns(X_train);
-num_ocultas = 5;
+num_ocultas = 6;
 num_etiquetas = length(unique(y_train));
 
 lambda = 1;
@@ -23,47 +23,52 @@ Theta2 = pesosAleatorios(num_ocultas, num_etiquetas);
 params_rn_ini = [Theta1(:); Theta2(:)];
 options = optimset('MaxIter', 250);
 
-values = [0.01, 0.1, 0.2, 0.3, 1, 3, 0];
+values = [0];
 lambda = 0;
 
 maxAccu = 0;
 bestlambda = 0;
 auxCost = 0;
 
-%for i = 1:columns(values)
-%	lambda = values(i);
-%	cost = @(t) costeRN(t, num_entradas, num_ocultas, num_etiquetas, X_train, y_train, lambda);
-%    [params_rn, J] = fmincg(cost, params_rn_ini, options);
-%	Theta11 = reshape(params_rn(1:num_ocultas * (num_entradas + 1)), num_ocultas, (num_entradas + 1));
-%    Theta21 = reshape(params_rn((1 + (num_ocultas * (num_entradas + 1))):end), num_etiquetas, (num_ocultas + 1));
-%    accu = accuracyNN(Theta11, Theta21, X_val, y_val);
-%	if accu > maxAccu
-%		maxAccu = accu;
-%		bestlambda = lambda;
-%		cost = auxCost;
-%	end
-%end
-
-%m = rows(X_train)/100;
-m = 100;
-for i = 1 : m
-	%row = 1:min(i*100,rows(X_train));
-	row = 1:i;
-    cost = @(t) costeRN(t, num_entradas, num_ocultas, num_etiquetas, X_train(row, :), y_train(row, :), lambda);
+for i = 1:columns(values)
+	lambda = values(i);
+	cost = @(t) costeRN(t, num_entradas, num_ocultas, num_etiquetas, X_train, y_train, lambda);
     [params_rn, J] = fmincg(cost, params_rn_ini, options);
-    % error
-    J_train(i) = costeRN(params_rn, num_entradas, num_ocultas, num_etiquetas, X_train(row, :), y_train(row, :), lambda);
-    Jval(i) = costeRN(params_rn, num_entradas, num_ocultas, num_etiquetas, X_val, y_val, lambda);
-endfor
+	Theta11 = reshape(params_rn(1:num_ocultas * (num_entradas + 1)), num_ocultas, (num_entradas + 1));
+    Theta21 = reshape(params_rn((1 + (num_ocultas * (num_entradas + 1))):end), num_etiquetas, (num_ocultas + 1));
+    accu = accuracyNN(Theta11, Theta21, X_val, y_val);
+	if accu > maxAccu
+		maxAccu = accu;
+		bestlambda = lambda;
+		bestcost = J;
+	end
+end
 
-plot(1:m, J_train, 1:m, Jval);
-legend("Train", "Val");
-axis([0 12 0 150]);
-pause;
+fprintf("Cost = %f\n", bestcost);
+fprintf("Accuracy %f with lambda = %f\n", maxAccu, bestlambda);
+pause
+
+%m = rows(X_train)/200;
+%%m = 100;
+%for i = 1 : m
+%	row = 1:min(i*200,rows(X_train));
+%	%row = 1:i;
+%    cost = @(t) costeRN(t, num_entradas, num_ocultas, num_etiquetas, X_train(row, :), y_train(row, :), lambda);
+%    [params_rn, J] = fmincg(cost, params_rn_ini, options);
+%    % error
+%    J_train(i) = costeRN(params_rn, num_entradas, num_ocultas, num_etiquetas, X_train(row, :), y_train(row, :), lambda);
+%    Jval(i) = costeRN(params_rn, num_entradas, num_ocultas, num_etiquetas, X_val, y_val, lambda);
+%endfor
+%
+%plot(1:m, J_train, 1:m, Jval);
+%legend("Train", "Val");
+%axis([0 12 0 150]);
+%pause;
 
 cost = @(t) costeRN(t, num_entradas, num_ocultas, num_etiquetas, X_train, y_train, bestlambda);
 [params_rn, J] = fmincg(cost, params_rn_ini, options);
 Theta11 = reshape(params_rn(1:num_ocultas * (num_entradas + 1)), num_ocultas, (num_entradas + 1));
 Theta21 = reshape(params_rn((1 + (num_ocultas * (num_entradas + 1))):end), num_etiquetas, (num_ocultas + 1));
 accu = accuracyNN(Theta11, Theta21, X_test, y_test);
+fprintf("Cost = %f\n", J);
 fprintf("Accuracy %f with lambda = %f\n", accu, bestlambda);
--- regresionLogistica/proyecto.m
+++ regresionLogistica/proyecto.m
@@ -1,11 +1,11 @@
-X_train = dlmread('../datos/xtrain.csv');
-y_train = dlmread('../datos/ytrain.csv');
+X_train = dlmread('../DatosAgrupados/xtrain.csv');
+y_train = dlmread('../DatosAgrupados/ytrain.csv');
 
-X_val = dlmread('../datos/xval.csv');
-y_val = dlmread('../datos/yval.csv');
+X_val = dlmread('../DatosAgrupados/xval.csv');
+y_val = dlmread('../DatosAgrupados/yval.csv');
 
-X_test = dlmread('../datos/xtest.csv');
-y_test = dlmread('../datos/ytest.csv');
+X_test = dlmread('../DatosAgrupados/xtest.csv');
+y_test = dlmread('../DatosAgrupados/ytest.csv');
 
 ## REGRESION LOGISTICA
 X_train = [ones(length(y_train), 1), X_train];
@@ -15,13 +15,14 @@ theta_inicial = zeros(columns(X_train), 1);
 
 opciones = optimset('GradObj', 'on', 'MaxIter', 500);
 [theta, cost] = fminunc(@(t)(costeLog(t, X_train, y_train)), theta_inicial, opciones);
-fprintf("Coste regresion logistica: %f\n", cost);
+fprintf("Coste regresion logistica sin regularizar: %f\n", cost);
 
-%m = rows(X_train)/100;
-m = 100;
+## CURVAS DE APRENDIZAJE
+m = rows(X_train)/200;
+%m = 100;
 for i = 1 : m
-	%row = 1:min(i*100,rows(X_train));
-	row = 1:i;
+	row = 1:min(i*200,rows(X_train));
+	%row = 1:i;
 	cost = @(t) costeLogReg(t, X_train(row, :), y_train(row, :), 0);
     theta = fminunc(cost, theta_inicial, opciones);
     % error
@@ -35,10 +36,10 @@ axis([0 12 0 150]);
 pause;
 
 accu = accuracyLog(theta, X_test, y_test);
-fprintf("Accuracy regresion logistica: %f\n", accu);
+fprintf("Accuracy regresion logistica sin regularizar: %f\n", accu);
 fprintf("\n");
 
-## REGRESION LOGISTICA REGULARIZADA
+%## REGRESION LOGISTICA REGULARIZADA MEJOR LAMBDA
 values = [0.01, 0.1, 0.2, 0.3, 1, 3];
 lambda = 0;
 
@@ -48,9 +49,13 @@ auxTheta = theta_inicial;
 auxCost = 0;
 
 for i = 1:columns(values)
+
 	lambda = values(i);
 	[auxTheta, auxCost] = fminunc(@(t)(costeLogReg(t, X_train, y_train, lambda)), theta_inicial, opciones);
-	accu = accuracyLog(auxTheta, X_val, y_val);
+	accu = accuracyLog(auxTheta, X_val, y_val)
+	Jreg(i) = costeLogReg(auxTheta, X_train(2:end, :), y_train(2:end, :), lambda);
+	Jvalreg(i) = costeLogReg(auxTheta, X_test, y_test, lambda);
+
 	if accu > maxAccu
 		maxAccu = accu;
 		bestlambda = lambda;
@@ -65,4 +70,3 @@ func = @(t)(costeLogReg(t, X_train, y_train, bestlambda));
 accu = accuracyLog(theta, X_test, y_test);
 fprintf("Mejor lambda: %f\n", bestlambda);
 fprintf("Accuracy regresion logistica reg: %f\n", accu);
-
